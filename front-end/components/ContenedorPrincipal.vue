277657e192ade8781a7381d1c0c2650475a29636
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-01 18:00:37 -0500

    Añadir logo en SVG

--- front-end/components/ContenedorPrincipal.vue
+++ front-end/components/ContenedorPrincipal.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <main class="flex gap-16 h-screen justify-center items-center bg-blue-800 text-neutral-900">
+  <main class="flex gap-16 h-screen justify-center items-center bg-stone-100 text-neutral-900">
     <Tarjetas divisa="US Dolar" :valor="4500" />
     <Tarjetas divisa="Bitcoin" :valor="45000000"/>
   </main>
--- front-end/components/Tarjetas.vue
+++ front-end/components/Tarjetas.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex bg-blue-500 text-slate-50 w-1/4 h-3/6 rounded-xl flex-col justify-center items-center">
+  <div class="flex bg-emerald-500 text-slate-50 w-1/4 h-3/6 rounded-xl flex-col justify-center items-center">
     <h1 class="text-3xl mb-4">{{divisa}} </h1>
     <h2 class="text-4xl mb-4">$ {{valor}}</h2>
   </div>
