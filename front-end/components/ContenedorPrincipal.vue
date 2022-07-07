92aa46707526e15db51f905a809e29bf909ff91a
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-06 23:08:46 -0500

    Ajustar los estilos version movil

--- front-end/components/ContenedorPrincipal.vue
+++ front-end/components/ContenedorPrincipal.vue
@@ -1,12 +1,11 @@
 <script lang="ts" setup></script>
 
 <template>
-  <main class="flex flex-col items-center justify-center w-screen h-screen bg-stone-100">
-    <div class="flex flex-col items-center justify-center w-screen">
+  <main class="w-screen h-screen bg-stone-100">
+    <div class="flex flex-col items-center justify-center w-screen h-1/3 bg-slate-100">
       <img class="w-40" src="../images/b2c-logo.svg" />
-      <p>Convertidor de divisas electronicas y extranjeras a pesos colombianos.</p>
     </div>
-    <div class="flex flex-row gap-16 items-center justify-center w-screen">
+    <div class="flex flex-col gap-8 items-center justify-center w-screen h-4/6 bg-slate-100">
       <Tarjetas divisa="US Dollar" :valor="4500" />
       <Tarjetas divisa="Bitcoin" :valor="45000000" />
     </div>
--- front-end/components/Tarjetas.vue
+++ front-end/components/Tarjetas.vue
@@ -12,8 +12,8 @@ defineProps({
 </script>
 
 <template>
-  <div class="w-1/5">
-    <div class="text-gray-200 py-6 px-0.5 text-5xl font-bold text-center  bg-sky-600 font-workSans rounded-t-md">
+  <div class="w-8/12">
+    <div class="py-6 text-gray-200 text-5xl font-bold text-center bg-sky-600 font-workSans rounded-t-md">
       ${{ valor }}
     </div>
     <div class="p-2 text-2xl font-light text-center bg-green-500 font-heebo rounded-b-md">{{ divisa }}</div>
