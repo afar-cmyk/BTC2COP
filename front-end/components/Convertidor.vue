1d8cbae405d3c1de1d093045ff41f10624fb8d49
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-09 01:41:52 -0500

    Ajustar el comportamiento del Convertidor

--- front-end/components/Convertidor.vue
+++ front-end/components/Convertidor.vue
@@ -7,8 +7,8 @@ defineProps({
 })
 
 let valorIngresado = ref<number>(0);
-const dollar: number = 4500;
-const bitcoin: number = 10000;
+const arrayPrueba = ref({ '': 1, dollar: 4500, bitcoin: 40000 })
+const valorSeleccionado = ref('')
 
 </script>
 
@@ -16,20 +16,20 @@ const bitcoin: number = 10000;
   <form>
     <label>
       <p class="font-heebo font-light mt-2 text-sm text-slate-70 text-center">
-        Seleccione una divisa e ingrese la cantidad que desea convertir a pesos Colombianos.
+        Seleccione una divisa, luego ingrese la cantidad que desea convertir a pesos Colombianos.
       </p>
       <div class=" flex flex-row justify-center align-middle gap-4">
-        <select name="divisas" id="seleccionar-divisa">
-          <option value="">💵 Divisas 💵</option>
-          <option value="dolar">Dolar</option>
+        <select v-model="valorSeleccionado" name="divisas" id="seleccionar-divisa">
+          <option disabled value="">💵 Divisas 💵</option>
+          <option value="dollar">Dolar</option>
           <option value="bitcoin">Bitcoin</option>
         </select>
-        <input v-model.number="valorIngresado"
+        <input :disabled="valorSeleccionado == ''" v-model.number="valorIngresado"
           class="border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
       </div>
     </label>
   </form>
-  <Tarjetas :valor="valorIngresado * dollar" divisa="Peso Colombiano" />
+  <Tarjetas :valor="valorIngresado * arrayPrueba[valorSeleccionado]" divisa="Pesos Colombianos" />
 </template>
 
 <style scoped>
