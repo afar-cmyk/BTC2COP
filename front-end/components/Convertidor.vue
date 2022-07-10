b30526b619d0901b565b5306299af54992927e60
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-10 02:00:56 -0500

    Añadir botones para seleccionar la divisa

--- front-end/components/Convertidor.vue
+++ front-end/components/Convertidor.vue
@@ -10,25 +10,40 @@ let valorIngresado = ref<number>(0);
 const arrayPrueba = ref({ '': 1, dollar: 4500, bitcoin: 40000 })
 const valorSeleccionado = ref('')
 
+//150000000 input maximo
 </script>
 
 <template>
+  <p class="font-heebo font-light mt-2 text-sm text-slate-70 text-center">
+    Seleccione una divisa, luego ingrese la cantidad que desea convertir a pesos Colombianos.
+  </p>
+
+  <ul class="grid grid-cols-2 gap-x-5 max-w-md mx-auto">
+    <li class="relative ">
+      <input v-model="valorSeleccionado" class="sr-only peer" type="radio" value="dollar" name="answer"
+        id="divisa_dollar">
+      <label
+        class="flex justify-center py-1 px-2 font-regular font-workSans bg-stone-100 text-sky-700  rounded-md cursor-pointer focus:outline-none hover:bg-stone-50 peer-checked:text-green-600 peer-checked:ring-green-500 peer-checked:ring-2"
+        for="divisa_dollar">Dollar</label>
+    </li>
+    <li class="relative ">
+      <input v-model="valorSeleccionado" class="sr-only peer" type="radio" value="bitcoin" name="answer"
+        id="divisa_bitcoin">
+      <label
+        class="flex justify-center py-1 px-2 font-regular font-workSans bg-stone-100 text-sky-700 rounded-md cursor-pointer focus:outline-none hover:bg-stone-50 peer-checked:text-yellow-600 peer-checked:ring-yellow-500 peer-checked:ring-2"
+        for="divisa_bitcoin">Bitcoin</label>
+    </li>
+  </ul>
+
   <form>
     <label>
-      <p class="font-heebo font-light mt-2 text-sm text-slate-70 text-center">
-        Seleccione una divisa, luego ingrese la cantidad que desea convertir a pesos Colombianos.
-      </p>
       <div class=" flex flex-row justify-center align-middle gap-4">
-        <select v-model="valorSeleccionado" name="divisas" id="seleccionar-divisa">
-          <option disabled value="">💵 Divisas 💵</option>
-          <option value="dollar">Dolar</option>
-          <option value="bitcoin">Bitcoin</option>
-        </select>
         <input :disabled="valorSeleccionado == ''" v-model.number="valorIngresado"
           class="border-slate-200 placeholder-slate-400 contrast-more:border-slate-400 contrast-more:placeholder-slate-500" />
       </div>
     </label>
   </form>
+
   <Tarjetas :valor="valorIngresado * arrayPrueba[valorSeleccionado]" divisa="Pesos Colombianos" />
 </template>
 
