a59ef2cc5919cfdb7b23690758bdf328f84ca1e5
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-19 01:54:29 -0500

    Añadir el codigo que regresa los valores del USD y BTC

--- scrapper/functions/api.js
+++ scrapper/functions/api.js
@@ -2,7 +2,12 @@ const chromium = require("chrome-aws-lambda");
 const puppeteer = require("puppeteer-core");
 
 exports.handler = async function (event, context) {
-  const browser = await puppeteer.launch({
+  let xpath = '//*[@id="quote-page-strip"]/div[3]/div/div[2]/span[1]';
+  let COP = "COP=";
+  let BTC = "BTC.CB=";
+  const URL = (endopoint) => `https://www.cnbc.com/quotes/${endopoint}`;
+
+  const explorador = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath:
@@ -11,21 +16,35 @@ exports.handler = async function (event, context) {
     ignoreHTTPSErrors: true,
   });
 
-  const page = await browser.newPage();
+  const pagina = await explorador.newPage();
 
-  await page.goto("https://v3.nuxtjs.org/guide/concepts/server-engine/");
+  await pagina.goto(URL(COP));
+  await pagina.waitForXPath(xpath);
 
-  const title = await page.title();
+  const [cop] = await pagina.$x(xpath);
+  const copContenido = await cop.getProperty("textContent");
+  const copCrudo = await copContenido.jsonValue();
 
-  await browser.close();
+  await pagina.goto(URL(BTC));
+  await pagina.waitForXPath(xpath);
+
+  const [btc] = await pagina.$x(xpath);
+  const btcContenido = await btc.getProperty("textContent");
+  const btcCrudo = await btcContenido.jsonValue();
+
+  await explorador.close();
+
+  const tratarValores = (valores) => {
+    let separarComa = valores.split(".");
+    let ponerPunto = parseInt(separarComa[0].replace(",", ""));
+    return ponerPunto;
+  };
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      status: "Ok",
-      page: {
-        title,
-      },
+      btc: tratarValores(btcCrudo),
+      cop: tratarValores(copCrudo),
     }),
   };
 };
