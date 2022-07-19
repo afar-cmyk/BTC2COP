ef5d1c90b8fedb96f421f82215114ed1a159dbe4
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-18 19:16:44 -0500

    Prueba de deploy 2

---
+++ scrapper/functions/api.js
@@ -0,0 +1,29 @@
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
+
+exports.handler = async function (event, context) {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+    headless: true,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("https://v3.nuxtjs.org/guide/concepts/server-engine/");
+
+  const title = await page.title();
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      status: "Ok",
+      page: {
+        title,
+      },
+    }),
+  };
+};
