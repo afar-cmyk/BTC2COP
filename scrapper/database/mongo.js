9c7c69574dd3d08960ad8ff2748479bb8aea2e93
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-20 23:52:04 -0500

    Añadir prueba mongo driver

--- scrapper/database/mongo.js
+++ scrapper/database/mongo.js
@@ -1,13 +1,61 @@
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
-let password = process.env.DATABASE_PASSWORD;
-const connectionString = `mongodb+srv://db_generica:${password}@cluster0.vsv0u.mongodb.net/Divisas?retryWrites=true&w=majority`;
+let dbCacheada = null;
 
-mongoose
-  .connect(connectionString)
-  .then(() => {
-    console.log("Base de datos conectada!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+async function conectarDb() {
+  if (dbCacheada) {
+    console.log("Utilizando la base de datos en cache");
+    return Promise.resolve(dbCacheada);
+  } else {
+    return MongoClient.connect(process.env.MONGODB_URI)
+      .then((cliente) => {
+        let db = cliente.db("NOMBREBASEDATOS");
+        console.log("Nueva conexion a la base de datos");
+        dbCacheada = db;
+        return dbCacheada;
+      })
+      .catch((error) => {
+        console.log("Error en la conexion!");
+        console.log(error);
+      });
+  }
+}
+
+module.exports = {
+  conectarDb,
+};
+
+// const { MongoClient } = require("mongodb");
+
+// let dbActual;
+
+// module.exports = {
+//   conexionDb: (callback) => {
+//     MongoClient.connect(
+//       `mongodb+srv://db_generica:${process.env.DATABASE_PASSWORD}@cluster0.vsv0u.mongodb.net/Divisas?retryWrites=true&w=majority`
+//     )
+//       .then((valores) => {
+//         dbActual = valores.db();
+//         return callback();
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         return callback(err);
+//       });
+//   },
+//   obtenerDb: () => dbActual,
+// };
+
+// const mongoose = require("mongoose");
+
+// let password = process.env.DATABASE_PASSWORD;
+// const connectionString = `mongodb+srv://db_generica:${password}@cluster0.vsv0u.mongodb.net/Divisas?retryWrites=true&w=majority`;
+
+// mongoose
+//   .connect(connectionString)
+//   .then(() => {
+//     console.log("Base de datos conectada!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
--- scrapper/functions/api.js
+++ scrapper/functions/api.js
@@ -1,36 +1,83 @@
-// require("../database/mongo.js");
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
-const connectionString = `mongodb+srv://db_generica:${process.env.DATABASE_PASSWORD}@cluster0.vsv0u.mongodb.net/Divisas?retryWrites=true&w=majority`;
+let dbCacheada = null;
 
-exports.handler = async function (event, context, callback) {
-  mongoose
-    .connect(connectionString)
-    .then(() => {
-      console.log("Base de datos conectada!");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+async function conectarDb() {
+  if (dbCacheada) {
+    console.log("Utilizando la base de datos en cache");
+    return Promise.resolve(dbCacheada);
+  } else {
+    return MongoClient.connect(process.env.MONGODB_URI)
+      .then((cliente) => {
+        let db = cliente.db("Divisas");
+        console.log("Nueva conexion a la base de datos");
+        dbCacheada = db;
+        return dbCacheada;
+      })
+      .catch((error) => {
+        console.log("Error en la conexion!");
+        console.log(error);
+      });
+  }
+}
 
-  const Valores = require("../models/Valores");
+exports.handler = async function (event, context) {
+  const db = await conectarDb();
+  const collection = await db.collection("valores");
 
-  const nuevosValores = new Valores({
-    cop: "prueba7",
-    btc: "prueba7",
-  });
-
-  nuevosValores
-    .save()
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-
+  const asddd = await collection.find({}).toArray();
+  console.log(asddd);
   return {
     statusCode: 200,
-    body: "Se agrego un dato satisfactoriamente!",
+    body: JSON.stringify(asddd),
   };
 };
+
+// const mongo = require("../Database/mongo");
+
+// exports.handler = async function (event, context) {
+//   const db = await mongo.conectarDb();
+//   const collection = await db.collection("Divisas");
+
+//   const users = await collection.find({}).toArray();
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       prueba: users,
+//     }),
+//   };
+// };
+
+// // require("../database/mongo.js");
+
+// const { conexionDb, obtenerDb } = require("../database/mongo");
+
+// let baseDatos;
+
+// conexionDb((err) => {
+//   if (!err) {
+//     console.log(`Conectado a la base de datos!`);
+//     baseDatos = obtenerDb();
+//   }
+// });
+
+// exports.handler = async function (event, context, callback) {
+//   let prueba = [];
+
+//   baseDatos
+//     .collection("valores")
+//     .find()
+//     .sort({ cop: 1 })
+//     .forEach((pruebas) => pruebas.push(prueba))
+//     .then(() => {
+//       res.status(200).json(prueba);
+//     })
+//     .catch(() => {
+//       res.status(500).json({ error: "No se pudo obtener la informacion" });
+//     });
+
+//   return {
+//     statusCode: 200,
+//     body: "Se agrego un dato satisfactoriamente!",
+//   };
+// };
