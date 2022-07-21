a8b05952cd86ac429f4113e63f3192e9390a65ce
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-07-20 23:59:28 -0500

    Ajustar funcion

--- scrapper/functions/api.js
+++ scrapper/functions/api.js
@@ -22,6 +22,27 @@ async function conectarDb() {
 }
 
 exports.handler = async function (event, context) {
+  let dbCacheada = null;
+
+  async function conectarDb() {
+    if (dbCacheada) {
+      console.log("Utilizando la base de datos en cache");
+      return Promise.resolve(dbCacheada);
+    } else {
+      return MongoClient.connect(process.env.MONGODB_URI)
+        .then((cliente) => {
+          let db = cliente.db("Divisas");
+          console.log("Nueva conexion a la base de datos");
+          dbCacheada = db;
+          return dbCacheada;
+        })
+        .catch((error) => {
+          console.log("Error en la conexion!");
+          console.log(error);
+        });
+    }
+  }
+
   const db = await conectarDb();
   const collection = await db.collection("valores");
 
